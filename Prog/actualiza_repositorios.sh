8389e7a6d0ec47bbac4029031f7fd3275bf82bdc
Luis Ferreira <luisferreira@iescastelar.com>
2022-11-02 09:17:33 +0100

    Add loggin de cambios

--- Prog/actualiza_repositorios.sh
+++ Prog/actualiza_repositorios.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
 # Limpiamos y restauramos pwd
-trap "echo Saliendo...; cd ${RAIZ}; exit" SIGINT
+_limpiamos(){
+	echo Saliendo...;
+	cd ${RAIZ};
+	rm ~/salida.txt;
+	rm ~/error.txt;
+	exit
+}
+trap _limpiamos SIGINT
+
+# CONSTANTES
+Reset='\e[0m'
+Bright='\e[1m'
+Dim='\e[2m'
+Underscore='\e[4m'
+Blink='\e[5m'
+Reverse='\e[7m'
+Hidden='\e[8m'
+
+FgBlack='\e[30m'
+FgRed='\e[31m'
+FgGreen='\e[32m'
+FgYellow='\e[33m'
+FgBlue='\e[34m'
+FgMagenta='\e[35m'
+FgCyan='\e[36m'
+FgWhite='\e[37m'
+
+BgBlack='\e[40m'
+BgRed='\e[41m'
+BgGreen='\e[42m'
+BgYellow='\e[43m'
+BgBlue='\e[44m'
+BgMagenta='\e[45m'
+BgCyan='\e[46m'
+BgWhite='\e[47m'
 
 # Salvamos pwd
 RAIZ="$PWD"
@@ -9,13 +43,39 @@ RAIZ="$PWD"
 # 	@param DIR recibe el directorio absoluto o relativo donde buscar
 #	Recorre todo el DIR realizando `git pull` de todos los repos contenidos en él
 _actualiza_repo(){
+	echo ""
+	echo "==================================================="
+	echo -e "\t \t $1"
+	echo "==================================================="
+	sleep 1
 	for F in `ls $1/` ; do
-		echo "-----------------------------------------------"
-		echo "Actualizando $F"
-		cd "$PWD/${1}/${F}"
-		git pull
-		git push
-		cd $RAIZ
+		if [[ -d $1 ]]; then
+			cd "$PWD/${1}/${F}"
+
+			git pull 1>"$HOME/salida.txt" 2> /dev/null;
+			salida=$(cat ~/salida.txt);
+			if [[ 'Ya está actualizado.' == "$salida" ]]; then
+				echo -e "\t $F sin cambios."
+			else
+				LOG="$HOME/repos/.${1}-${F}.log"
+				echo "Actualizando $F"
+				echo "$salida"
+				#echo "$(date) - cambios:" >> "$LOG"
+				#echo -e "\t $(grep -E 'changed|deletion|insertion' ~/salida.txt)" >> "$LOG"
+				echo -e "$(date), $(grep -E 'changed|deletion|insertion' ~/salida.txt)" >> "$LOG"
+			fi
+
+			# git pull
+			git push 1>"$HOME/salida.txt" 2> "$HOME/error.txt"
+			alDia=$(cat ~/error.txt);
+			if [[ 'Everything up-to-date' == "$alDia" ]]; then 
+				echo -e "\t \t Al día"
+			else
+				echo -e "\t \t $(cat $HOME/salida.txt)"
+				echo -e "\t \t $FgRed $(cat $HOME/error.txt) $Reset"
+			fi
+			cd $RAIZ
+		fi
 	done
 }
 
